5759cc3d07eec7a83551b5ef30ef98f0e89fb23f
yuraslivka <slivka.y@gmail.com>
2019-11-04 20:33:22 +0200

    04.11.19

--- 18/js/common.js
+++ 18/js/common.js
@@ -16,7 +16,74 @@ window.onload = function (){
     var outKkal = document.getElementById('kkal');
     outKkal.innerHTML = kkal + ' Kkal ';
   }
+  getResult();
 
+  /*-----------------Номер карты-------------------*/
+  document.querySelector('#card-number').oninput = function() {
+    let cardNum = this.value;
+    if(cardNum.trim().length>5){
+      let cardInfo = new CardInfo(cardNum.trim(),{
+        banksLogosPath: ' ./node_modules/card-info/dist/banks-logos/',
+        brandsLogosPath: ' ./node_modules/card-info/dist/brands-logos/'
+      });
+      console.log(cardInfo.bankName);
+      var banklogo = document.querySelector('.bank-logo');
+      banklogo.src = cardInfo.bankLogo;
+      var cardlogo = document.querySelector('.card-logo');
+      cardlogo.src = cardInfo.brandLogo;
+      var cardbg = document.querySelector('.card-front');
+      cardbg.style.background = cardInfo.backgroundColor;
+    }
+  }
+  /*-------------------Алгоритм Луна---------------------------------*/
+
+  document.querySelector('#card-number').oninput = function() {
+    let cardNumvalid = this.value;
+    valid_credit_card(cardNumvalid);
+    if (valid_credit_card(cardNumvalid)){
+    console.log(cardNumvalid+":", valid_credit_card(cardNumvalid));
+  }
+  }
+
+  function valid_credit_card(value) {
+    // accept only digits, dashes or spaces
+      if (/[^0-9-\s]+/.test(value)) return false;
+
+      // The Luhn Algorithm. It so pretty.
+      var nCheck = 0, nDigit = 0, bEven = false;
+      value = value.replace(/\D/g, "");
+
+      for (var n = value.length - 1; n >= 0; n--) {
+          var cDigit = value.charAt(n),
+                nDigit = parseInt(cDigit, 10);
+
+          if (bEven) {
+              if ((nDigit *= 2) > 9) nDigit -= 9;
+          }
+
+          nCheck += nDigit;
+          bEven = !bEven;
+      }
+
+      return (nCheck % 10) == 0;
+  }
+
+/*-------запуск в заданое время------------
+
+  function alertInfo(){
+    alert('BOO!!!');
+  };
+
+  function checkTime(){
+    var timeInSec = Math.round(Date.now()/1000); 
+    if(timeInSec > 1572891750){  /*<------сравнение с unix временем
+      alertInfo();
+    }
+   console.log(timeInSec);
+  }
+  checkTime();
+-----------------------------------------*/
+  
 }
 
   
